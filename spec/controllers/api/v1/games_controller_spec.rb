01d48b1c84018d9c0c425fb5f0d41c3fa308b2c8
Aaron Klaassen <aaron@outerspacehero.com>
2016-06-17 23:32:40 -0500

    API game controller specs

---
+++ spec/controllers/api/v1/games_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::GamesController, type: :controller do
+  let(:winnitron) { FactoryGirl.create(:arcade_machine) }
+  let(:token) { winnitron.api_keys.first.token }
+
+  describe "GET index" do
+
+    include_examples "disallows bad API keys", :get, :index
+
+    it "accepts API key in params" do
+      get :index, { api_key: token, format: "json" }
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "accepts API key in headers" do
+      request.headers["Authorization"] = "Token #{token}"
+      get :index, { format: "json" }
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "assigns all games" do
+      FactoryGirl.create_list(:game, 2)
+      
+      request.headers["Authorization"] = "Token #{token}"
+      get :index, { format: "json" }
+      expect(assigns(:games)).to eq Game.all
+    end
+
+  end
+
+end
---
+++ spec/support/disallows_bad_api_keys.rb
@@ -0,0 +1,28 @@
+shared_examples "disallows bad API keys" do |method, action|
+  let(:parameters) { defined?(params) ? params : {} }
+
+  describe "header key" do
+    it "returns 403 for blank key" do
+      public_send(method, action, parameters, {})
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it "returns 403 for non-existent key" do
+      request.headers["Authorization"] = "Token fakefakefake"
+      public_send(method, action, parameters)
+      expect(response).to have_http_status(:forbidden)
+    end
+  end
+
+  describe "parameter key" do
+    it "returns 403 for blank key" do
+      public_send(method, action, parameters.except(:api_key))
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it "returns 403 for non-existent key" do
+      public_send(method, action, parameters.merge(api_key: "fakefake"))
+      expect(response).to have_http_status(:forbidden)
+    end
+  end
+end
