2fb54107eb73174b24fb6f05193eb07e6ca16310
Aaron Klaassen <aaron@outerspacehero.com>
2016-06-18 12:37:25 -0500

    default api to json format

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "/users/:user_id/arcade_machines" => "arcade_machines#mine", as: :user_arcade_machines
 
 
-  namespace :api do
+  namespace :api, defaults: { format: "json" } do
     namespace :v1 do
       resources :games, only: [:index]
     end
