f18c412a42834e125883e527fa4405bb8ff66ddc
Aaron Klaassen <aaron@outerspacehero.com>
2016-07-23 15:08:29 -0500

    Add games controller pending specs.

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -3,5 +3,9 @@ FactoryGirl.define do
     title       { Faker::Lorem.words(3).join(" ") }
     description { Faker::Lorem.sentence(2) }
     zipfile_key { [Faker::Lorem.word, ".zip"].join }
+
+    after :create do |game|
+      GameOwnership.create game: game, user: FactoryGirl.create(:user)
+    end
   end
 end
