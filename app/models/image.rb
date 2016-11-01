721ddc512ab5f0c7541b042f3682d0170a4d88c7
Aaron Klaassen <aaron@outerspacehero.com>
2016-11-01 16:49:03 -0500

    Cleaned-up Arcade Machine show.

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ActiveRecord::Base
   belongs_to :parent, polymorphic: true
 
   def url
-    # TODO
-    "http://example.com/picture.jpg"
+    object = Aws::S3::Object.new(bucket_name: ENV["AWS_BUCKET"], key: file_key)
+    object.presigned_url(:get, expires_in: 1.hour)
   end
 end
