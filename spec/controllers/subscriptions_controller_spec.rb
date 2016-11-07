08217aec5bb04315f6acc29a1f2f7ce5707c5818
Aaron Klaassen <aaron@outerspacehero.com>
2016-11-06 18:48:47 -0600

    Fix exception when trying to delete a subscription. Fixes #31.

---
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe SubscriptionsController, type: :controller do
+  describe "POST create" do
+    pending
+  end
+
+  describe "DELETE destroy" do
+    pending
+  end
+end
