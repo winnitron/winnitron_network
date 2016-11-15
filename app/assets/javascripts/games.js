90be63c40f300575bd4b47f0ceb124fc11b93b17
Aaron Klaassen <aaron@outerspacehero.com>
2016-11-14 21:22:33 -0600

    Better arcade machine image uploader.

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -90,15 +90,5 @@
         $("#zipfile-uploader .btn").removeClass("disabled");
     });
 
-
-
-
-    $("#image-uploader").bind("s3_upload_complete", function(e, content) {
-      img = '<img src="' + content.url + '">';
-      link = '<a href="' + content.url + '" class="screenshot">' + img + '</a>';
-      $(".current-images").append(link)
-    })
-
-
   });
 })(window, jQuery)
