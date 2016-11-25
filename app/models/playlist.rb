e87144076734b673c470468862e403470fc5dfbc
Aaron Klaassen <aaron@outerspacehero.com>
2016-11-25 16:05:49 -0600

    Playlist cover image.

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,14 +1,25 @@
 class Playlist < ActiveRecord::Base
+
   validates :title, :user, presence: true
+  validate :title_doesnt_start_with_underscore
 
   belongs_to :user
 
   has_many :listings, dependent: :destroy
   has_many :games, through: :listings
 
-  # subscriptions
+  has_many :subscriptions, dependent: :destroy
+  has_many :arcade_machines, through: :subscriptions
 
   scope :defaults, -> { where(default: true) }
 
-  # TODO: title can't start with an underscore
+  def cover_image
+    games.map(&:cover_image).compact.first || Image.placeholder
+  end
+
+  private
+
+  def title_doesnt_start_with_underscore
+    errors.add(:title, "can't start with an underscore") if title && title[0] == "_"
+  end
 end
