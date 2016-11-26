b226c7a8674588b02889a1af4faf567e32e41dfc
Aaron Klaassen <aaron@outerspacehero.com>
2016-11-26 16:14:17 -0600

    Image removal.

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -2,11 +2,13 @@ require "rails_helper"
 
 RSpec.describe GamesController, type: :controller do
   describe "update" do
+    it "requires ownership"
     it "sets the cover image"
     it "ensures only one cover image"
   end
 
   describe "destroy" do
+    it "requires ownership"
     it "deletes the image"
   end
 end
