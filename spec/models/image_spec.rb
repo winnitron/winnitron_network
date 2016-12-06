fe851026b342a8f822a2de218c35095e7a8825b8
Aaron Klaassen <aaron@outerspacehero.com>
2016-12-05 22:08:43 -0600

    Use actually-resized images.

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe Image, type: :model do
   let(:image) { Image.new(file_key: "example.jpg") }
   let(:aws_url) { "https://#{ENV['AWS_BUCKET']}.s3-#{ENV['AWS_REGION']}.amazonaws.com/#{image.file_key}" }
 
+  before :each do
+    allow_any_instance_of(Aws::S3::Object).to receive(:public_url).and_return(aws_url)
+  end
+
   describe "#url" do
     it "without dimensions, it gives the original URL" do
       expect(image.url).to eq aws_url
