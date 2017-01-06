696dea4349eeefea30d6529ba29147e007e7f1e1
Aaron Klaassen <aaron@outerspacehero.com>
2017-01-06 17:36:34 -0600

    Add smart tags to Playlists.

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,8 @@
 class Playlist < ActiveRecord::Base
   include Slugged
 
+  acts_as_taggable_on :smart_tags
+
   validates :title, :user, presence: true
   validate :title_doesnt_start_with_underscore
 
@@ -18,7 +20,23 @@ class Playlist < ActiveRecord::Base
     games.map(&:cover_image).compact.first || Image.placeholder
   end
 
+  def smart?
+    smart_tags.any?
+  end
+
+  def games
+    smart? ? smart_games : super
+  end
+
   private
+
+  def smart_games
+    # Yes, this is inefficient but I'll worry about that when we have
+    # enough games for it to matter.
+    Game.with_zip.select do |g|
+      (g.tag_list & smart_tags.pluck(:name)).size == smart_tags.size
+    end
+  end
 
   def title_doesnt_start_with_underscore
     errors.add(:title, "can't start with an underscore") if title && title[0] == "_"
