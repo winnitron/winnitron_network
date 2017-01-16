32ba872eeaeabf7b775ed6292a56ff07ea672ced
Aaron Klaassen <aaron@outerspacehero.com>
2017-01-16 17:20:15 -0600

    Refactor Playlist smart game listing (ie save real Listing objects).

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -16,6 +16,8 @@ class Playlist < ActiveRecord::Base
 
   scope :defaults, -> { where(default: true) }
 
+  after_save :update_smart_listings, if: -> { smart? }
+
   def cover_image
     games.map(&:cover_image).compact.first || Image.placeholder
   end
@@ -24,21 +26,21 @@ class Playlist < ActiveRecord::Base
     smart_tags.any?
   end
 
-  def games
-    smart? ? smart_games : super
-  end
-
   private
-
-  def smart_games
-    # Yes, this is inefficient but I'll worry about that when we have
-    # enough games for it to matter.
-    Game.with_zip.select do |g|
-      (g.tag_list & smart_tags.pluck(:name)).size == smart_tags.size
-    end
-  end
 
   def title_doesnt_start_with_underscore
     errors.add(:title, "can't start with an underscore") if title && title[0] == "_"
+  end
+
+  def update_smart_listings
+    listings.destroy_all
+
+    # Yes, this is inefficient but I'll worry about that when we have
+    # enough games for it to matter.
+    games = Game.with_zip.select do |g|
+      (g.tag_list & smart_tags.pluck(:name)).size == smart_tags.size
+    end
+
+    games.each { |game| listings.create(game: game) }
   end
 end
---
+++ spec/models/playlist_spec.rb
@@ -0,0 +1,50 @@
+require "rails_helper"
+
+RSpec.describe Playlist, type: :model do
+  describe "smart updates" do
+
+    context "it's not smart" do
+      let(:playlist) { Playlist.new(title: "Dumb", user: FactoryGirl.build(:user)) }
+
+      it "does shit all" do
+        playlist.save
+        expect(playlist.listings).to be_empty
+      end
+    end
+
+    context "it's smart" do
+      let(:playlist) do
+        p = Playlist.new(title: "Dumb", user: FactoryGirl.build(:user))
+        p.smart_tag_list = "good, bad, ugly"
+        p
+      end
+
+      let!(:matching) do
+        games = FactoryGirl.create_list(:game, 3)
+        games.each do |game|
+          game.tag_list = ["good", "bad", "ugly", Faker::Lorem.word].join(",")
+          game.save
+        end
+
+        games
+      end
+
+      let!(:non_matching) do
+        g = FactoryGirl.create(:game)
+        g.tag_list = "good, foo, bar"
+        g
+      end
+
+      it "creates listings for all matching games" do
+        playlist.save
+        expect(playlist.games).to match_array matching
+      end
+
+      it "does not include non-matching games" do
+        playlist.save
+        expect(playlist.games).to_not include(non_matching)
+      end
+    end
+
+  end
+end
