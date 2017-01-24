463acf2c6745b6bbfc0b04f4a7904ae772f78b87
Aaron Klaassen <aaron@outerspacehero.com>
2017-01-23 22:40:28 -0600

    Change game form popular tag links to add tag rather than go to search.

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -40,5 +40,12 @@
         $("#zipfile-uploader .btn").removeClass("disabled");
     });
 
+
+    $(".popular-tags .js-add-tag").click(function(e) {
+      e.preventDefault();
+      var field = $(".js-tag-list")
+      field.tagsinput("add", $(this).attr("href"))
+    })
+
   });
 })(window, jQuery)
