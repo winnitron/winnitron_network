4dc040985bb0bc193edd437aa2608e0095a07046
Aaron Klaassen <aaron@outerspacehero.com>
2017-04-04 14:00:40 -0500

    Rename CallbacksController to OmniauthCallbacksController.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: "registrations",
-    omniauth_callbacks: "callbacks"
+    omniauth_callbacks: "omniauth_callbacks"
   }
 
   resources :games do
