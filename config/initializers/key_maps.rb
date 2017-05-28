88b6b88e6ec3d9f81e176e1698defa6892c322e7
Aaron Klaassen <aaron@outerspacehero.com>
2017-05-28 15:48:58 -0500

    Add the rest of the key map templates.

--- config/initializers/key_maps.rb
+++ config/initializers/key_maps.rb
@@ -1,1 +1,2 @@
-KEY_MAP_TEMPLATES = YAML.load(File.read(File.expand_path('../../key_map_templates.yml', __FILE__))).with_indifferent_access
+file = File.read(File.expand_path('../../key_map_templates.yml', __FILE__))
+KEY_MAP_TEMPLATES = YAML.load(file).with_indifferent_access
