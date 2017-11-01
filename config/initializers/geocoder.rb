dc56a9770d45088968c7d9a2960d8f6ec4f78018
Aaron Klaassen <aaron@outerspacehero.com>
2017-11-01 14:20:26 -0500

    Add geocoder initializer.

---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,22 @@
+Geocoder.configure(
+  # Geocoding options
+  api_key: ENV["GOOGLE_MAPS_API_KEY"],
+  language: :en,
+  lookup: :google,
+  use_https: true,
+
+  # timeout: 3,                 # geocoding service timeout (secs)
+  # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
+  # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
+  # cache: nil,                 # cache object (must respond to #[], #[]=, and #keys)
+  # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
+
+  # Exceptions that should not be rescued by default
+  # (if you want to implement custom error handling);
+  # supports SocketError and TimeoutError
+  # always_raise: [],
+
+  # Calculation options
+  # units: :mi,                 # :km for kilometers or :mi for miles
+  # distances: :linear          # :spherical or :linear
+)
