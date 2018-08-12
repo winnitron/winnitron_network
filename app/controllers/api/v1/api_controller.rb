42df95e778ff9e200f140f348cb87c79abaf40c6
Aaron Klaassen <aaron@outerspacehero.com>
2018-08-12 15:17:01 -0500

    Render errors when API auth fails.

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -9,21 +9,27 @@ class Api::V1::ApiController < ApplicationController
   def token_authentication!
     auth = RequestAuthenticator.new(request, requires_signature: false)
     if auth.valid?
-      @client = auth.key.parent
-      @arcade_machine = @client # TODO: but maybe it's a game!
+      set_client(auth)
     else
-      head :forbidden
+      render json: { errors: ["Invalid API key."] }, status: :forbidden
     end
   end
 
   def signed_authentication!
     auth = RequestAuthenticator.new(request, requires_signature: true)
     if auth.valid?
-      @client = auth.key.parent
-      @arcade_machine = @client # TODO: but maybe it's a game!
+      set_client(auth)
     else
-      head :forbidden
+      render json: { errors: ["Invalid API key or signature."] }, status: :forbidden
     end
   end
 
+  def set_client(auth)
+    client = auth.key.parent
+    if client.is_a?(ArcadeMachine)
+      @arcade_machine = client
+    elsif client.is_a?(Game)
+      @game = client
+    end
+  end
 end
