1dc8fe3587ab7526defdbe28ff2ac59246f3e3b3
Aaron Klaassen <aaron@outerspacehero.com>
2019-03-01 17:15:35 -0600

    Sortable and filterable high score page.

--- app/models/filtered_high_scores.rb
+++ app/models/filtered_high_scores.rb
@@ -4,7 +4,7 @@ class FilteredHighScores
   delegate :each, to: :@scores
 
   def initialize
-    @scores = HighScore.all
+    @scores = HighScore.all.order(score: :desc)
   end
 
   def game(game)
@@ -12,9 +12,14 @@ class FilteredHighScores
     self
   end
 
-  def arcade(machine)
-    machine = find_arcade_machine(machine)
-    @scores = machine ? @scores.where(arcade_machine: machine) : @scores
+  def arcade(machine_id)
+    @scores = if machine_id == "none"
+      @scores.where(arcade_machine: nil)
+    else
+      found = find_arcade_machine(machine_id)
+      found ? @scores.includes(:arcade_machine).where(arcade_machine: found) : @scores
+    end
+
     self
   end
 
@@ -29,6 +34,10 @@ class FilteredHighScores
       sort
     when "new"
       { id: :desc }
+    when "old"
+      { id: :asc }
+    when "low"
+      { score: :asc }
     else
       { score: :desc }
     end
@@ -36,7 +45,6 @@ class FilteredHighScores
     @scores = @scores.reorder(order_arg)
     self
   end
-
 
   private
 
